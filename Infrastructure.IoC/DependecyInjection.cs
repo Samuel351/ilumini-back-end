be852af836eb983557f080a5ab664a24de21580a
Samuel Silva Costa <72561919+Samuel351@users.noreply.github.com>
2024-10-12 12:53:17 -0300

    Feat: Infrasctructure IoC layer

---
+++ Infrastructure.IoC/DependecyInjection.cs
@@ -0,0 +1,38 @@
+﻿using Application.Repositories;
+using Application.Services.Implementations;
+using Application.Services.Interfaces;
+using Infraestructure.Data.Context;
+using Infraestructure.Data.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.IoC
+{
+    public static class DependecyInjection
+    {
+        public static void AddInfraestructure(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddScoped(typeof(IRepositoryBase<>), typeof(RepositoryBase<>));
+
+            services.AddDbContext<AppDbContext>(options => options.UseNpgsql(
+                Environment.GetEnvironmentVariable("DefaultConnection") ?? configuration.GetConnectionString("DefaultConnection"))
+            );
+        }
+
+        public static void AddServices(this IServiceCollection services)
+        {
+            services.AddScoped(typeof(IServiceBase<>), typeof(ServiceBase<>));
+        }
+
+        public static void AddUseCases(this IServiceCollection services)
+        { 
+        }
+    }
+}
